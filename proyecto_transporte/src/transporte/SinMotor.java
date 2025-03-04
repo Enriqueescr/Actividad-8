ec964c0187cc638665d78dc7411f746868eb0da8
Enriqueescr <enrique.escribano@bootcamp-upgrade.com>
2025-03-04 10:58:21 +0100

    Delete proyecto_transporte directory

--- proyecto_transporte/src/transporte/SinMotor.java
+++
@@ -1,9 +0,0 @@
-package proyecto_transporte.src.transporte;
-
-public interface SinMotor {
-  
-  static void usarFuerzaHumana(){
-    System.out.println("Usando fuerza humana para moverse");
-  }
-  
-}
